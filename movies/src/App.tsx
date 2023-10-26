2d9f286ff77e353365f83b3637e9e585d0ec7cc5
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-10-26 19:04:17 +0200

    feat: render & method update results

--- movies/src/App.tsx
+++ movies/src/App.tsx
@@ -1,14 +1,29 @@
+import { Component } from 'react';
 import './App.scss';
+import CatalogProducts from './CatalogProducts/CatalogProducts';
 import Header from './Header/Header';
+import IMovie from './Api/Api';
 
-function App() {
 
-  return (
-    <div className="banner_start">
-      <Header />
-      <h2 className="title"> We provide detailed descriptions of each film, trailers, ratings, and reviews to help you make an informed decision about which movie to watch.</h2>
-    </div>
-  );
+class App extends Component {
+  state = {
+    searchResults: [],
+  };
+
+  updateSearchResults = (results: IMovie[] | undefined) => {
+    this.setState({ searchResults: results || [] });
+    console.log("update", results)
+  }
+
+  render() {
+    return (
+      <div className="banner_start">
+        <Header updateSearchResults={this.updateSearchResults} />
+        <h2 className="title"> We provide detailed descriptions of each film, trailers, ratings, and reviews to help you make an informed decision about which movie to watch.</h2>
+        <CatalogProducts searchResults={this.state.searchResults} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- movies/src/Header/Header.tsx
+++ movies/src/Header/Header.tsx
@@ -1,12 +1,13 @@
-import Search from "../SearchBar/SearchBar"
-import './Header.scss'
-function Header() {
+import Search from "../SearchBar/SearchBar";
+import './Header.scss';
+
+function Header({ updateSearchResults }: any) {
     return (
         <div className="header">
             <h1><a href="#">Movies</a></h1>
-            <Search />
+            <Search updateSearchResults={updateSearchResults} />
         </div>
     )
 }
 
-export default Header
+export default Header;
