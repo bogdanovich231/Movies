e2f1fbbee694870449d40df1f935b83543418726
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-10-26 16:42:41 +0200

    feat: translation into class components and interfaces

--- movies/src/SearchBar/SearchBar.tsx
+++ movies/src/SearchBar/SearchBar.tsx
@@ -1,15 +1,33 @@
+import { Component } from "react";
 import search from "../assets/Vector.svg";
 import './SearchBar.scss';
 
-function Search() {
-    return (
-        <div className="search_bar">
-            <input placeholder="Search movie" type="text" />
-            <button className="search_btn">
-                <img src={search} alt="search btn" />
-            </button>
-        </div>
-    )
+interface SearchState {
+    query: string;
+    searchResults: IMovie | [] | undefined;
+}
+interface SearchProps {
+    updateSearchResults: (results: IMovie[] | undefined) => void;
+}
+class Search extends Component<SearchProps, SearchState> {
+    constructor(props: SearchProps) {
+        super(props);
+        this.state = {
+            query: '',
+            searchResults: [],
+        };
+    }
+
+    render() {
+        return (
+            <div className="search_bar" >
+                <input placeholder="Search movie" type="text" />
+                <button className="search_btn">
+                    <img src={search} alt="search btn" />
+                </button>
+            </div>
+        )
+    }
 }
 
 export default Search;
