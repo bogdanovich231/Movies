903be083de5151dee9db4e37944c844bea6c5047
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-10-27 20:29:23 +0200

    feat: create loading and styles

--- movies/src/App.tsx
+++ movies/src/App.tsx
@@ -1,9 +1,9 @@
-import { Component } from 'react';
+import { Component, Suspense, lazy } from 'react';
 import './App.scss';
-import CatalogProducts from './CatalogProducts/CatalogProducts';
 import Header from './Header/Header';
 import IMovie from './Api/Api';
-
+import Loading from './Loading/Loading';
+const CatalogProducts = lazy(() => import('./CatalogProducts/CatalogProducts'));
 
 class App extends Component {
   state = {
@@ -20,7 +20,9 @@ class App extends Component {
       <div className="banner_start">
         <Header updateSearchResults={this.updateSearchResults} />
         <h2 className="title"> We provide detailed descriptions of each film, trailers, ratings, and reviews to help you make an informed decision about which movie to watch.</h2>
-        <CatalogProducts searchResults={this.state.searchResults} />
+        <Suspense fallback={<Loading />}>
+          <CatalogProducts searchResults={this.state.searchResults} />
+        </Suspense>
       </div>
     );
   }
