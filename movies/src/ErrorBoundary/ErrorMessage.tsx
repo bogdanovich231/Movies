67b15e37e922459b75d2740d3edd11fae21514b2
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-10-29 18:00:30 +0100

    feat: create error boundery & style

---
+++ movies/src/ErrorBoundary/ErrorMessage.tsx
@@ -0,0 +1,11 @@
+import "./ErrorMessage.scss";
+
+function ErrorMessage() {
+    return (
+        <div className="message_boundary">
+            {'Something went wrong. Please, reload this page.'}
+        </div >
+    )
+}
+
+export default ErrorMessage;
--- movies/src/main.tsx
+++ movies/src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundery.tsx';
+import ErrorMessage from './ErrorBoundary/ErrorMessage.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallback={<ErrorMessage />}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
