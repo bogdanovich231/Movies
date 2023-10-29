3cbdc282c7d38e0a073b6e35bcb035716a55ed6e
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-10-29 19:01:36 +0100

    feat: create button error

---
+++ movies/src/ErrorBoundary/ShowError.tsx
@@ -0,0 +1,23 @@
+import { Component } from 'react'
+import "./ErrorMessage.scss";
+
+class ShowError extends Component<Record<string, never>, { showError: boolean }>{
+    constructor(props: Record<string, never>) {
+        super(props);
+        this.state = { showError: false }
+    }
+    showError = () => {
+        this.setState({ showError: true })
+    }
+    render() {
+        if (this.state.showError) {
+            throw new Error("Test error.")
+        }
+        return (
+            <button className="show_err_btn" onClick={this.showError}>Show Error</button>
+        )
+    }
+
+}
+
+export default ShowError;
