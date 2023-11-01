11a651e98a84cca85981445971f036263165d379
Tatsiana <kulinkovich56@gmail.com>
2023-11-01 15:20:58 +0100

    refactor: replacement by FC

--- movies/src/App.tsx
+++ movies/src/App.tsx
@@ -1,35 +1,31 @@
-import { Component, Suspense, lazy } from 'react';
+import {  Suspense, lazy, useState } from 'react';
 import './App.scss';
 import Header from './Header/Header';
 import IMovie from './Api/Api';
 import Loading from './Loading/Loading';
 const CatalogProducts = lazy(() => import('./CatalogProducts/CatalogProducts'));
 
-class App extends Component {
-  state = {
-    searchResults: [],
-    isLoading: false,
-  };
+function App() {
+  const [searchResults, setSearchResults] = useState<IMovie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  updateSearchResults = (results: IMovie[] | undefined) => {
-    this.setState({ searchResults: results || [] });
+  const updateSearchResults = (results: IMovie[] | undefined) => {
+    setSearchResults(results || []);
     console.log('update', results);
-  };
-
-  render() {
-    return (
-      <div className="banner_start">
-        <Header updateSearchResults={this.updateSearchResults} />
-        <h2 className="title">
-          We provide detailed descriptions of each film, trailers, ratings, and reviews to help you make an informed
-          decision about which movie to watch.
-        </h2>
-        <Suspense fallback={<Loading />}>
-          <CatalogProducts searchResults={this.state.searchResults} isLoading={this.state.isLoading} />
-        </Suspense>
-      </div>
-    );
   }
+
+  return (
+    <div className="banner_start">
+      <Header updateSearchResults={updateSearchResults} />
+      <h2 className="title">
+        We provide detailed descriptions of each film, trailers, ratings, and reviews to help you make an informed
+        decision about which movie to watch.
+      </h2>
+      <Suspense fallback={<Loading />}>
+        <CatalogProducts searchResults={searchResults} isLoading={isLoading} />
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
--- movies/src/SearchBar/SearchBar.tsx
+++ movies/src/SearchBar/SearchBar.tsx
@@ -1,53 +1,42 @@
-import { ChangeEvent, Component } from 'react';
+import { ChangeEvent, useState } from 'react';
 import search from '../assets/Vector.svg';
 import './SearchBar.scss';
 import IMovie, { searchMovie } from '../Api/Api';
 
-export interface SearchState {
-  query: string;
-  searchResults: IMovie | [] | undefined;
-  isLoading: boolean;
-}
 export interface SearchProps {
   updateSearchResults: (results: IMovie[] | undefined) => void;
 }
-class Search extends Component<SearchProps, SearchState> {
-  constructor(props: SearchProps) {
-    super(props);
-    this.state = {
-      query: localStorage.getItem('searchQuery') || '',
-      searchResults: [],
-      isLoading: false,
-    };
-  }
-  handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    this.setState({ query: event.target.value });
+
+function Search({ updateSearchResults }: SearchProps) {
+  const [query, setQuery] = useState<string>(localStorage.getItem('searchQuery') || '');
+  const [, setIsLoading] = useState(false);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
   };
-  handleSearch = async () => {
-    const { query } = this.state;
+
+  const handleSearch = async () => {
     localStorage.setItem('searchQuery', query);
-    this.setState({ isLoading: true });
+    setIsLoading(true);
+
     try {
       const result = (await searchMovie(query)) as IMovie[] | undefined;
-      console.log('Results from API:', result);
-      this.props.updateSearchResults(result || []);
+      updateSearchResults(result || []);
     } catch (error) {
       console.error('Error:', error);
     } finally {
-      this.setState({ isLoading: false });
+      setIsLoading(false);
     }
   };
 
-  render() {
-    return (
-      <div className="search_bar">
-        <input placeholder="Search movie" type="text" onChange={this.handleInputChange} value={this.state.query} />
-        <button className="search_btn" onClick={this.handleSearch}>
-          <img src={search} alt="search btn" />
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="search_bar">
+      <input placeholder="Search movie" type="text" onChange={handleInputChange} value={query} />
+      <button className="search_btn" onClick={handleSearch}>
+        <img src={search} alt="search btn" />
+      </button>
+    </div>
+  );
 }
 
 export default Search;
