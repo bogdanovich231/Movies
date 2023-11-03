b0eff77851aade8854b3a57c77b50a05f0cb2b20
Tatsiana <kulinkovich56@gmail.com>
2023-11-03 14:40:37 +0100

    feat: create router

--- movies/src/Header/Header.tsx
+++ movies/src/Header/Header.tsx
@@ -1,15 +1,17 @@
 import ShowError from '../ErrorBoundary/ShowError';
 import Search, { SearchProps } from '../SearchBar/SearchBar';
 import './Header.scss';
+import { Outlet, Link } from "react-router-dom";
 
 function Header({ updateSearchResults }: SearchProps) {
   return (
     <div className="header">
       <h1>
-        <a href="#">Movies</a>
+      <Link to={`/`}>Movies</Link>
       </h1>
       <Search updateSearchResults={updateSearchResults} />
       <ShowError />
+      <Outlet />
     </div>
   );
 }
--- movies/src/main.tsx
+++ movies/src/main.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.tsx';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundery.tsx';
-import ErrorMessage from './ErrorBoundary/ErrorMessage.tsx';
+import NotFound from './NotFound/NotFound.tsx';
+import ProductDetailed from './ProductDetailed/ProductDetailed.tsx';
+import CatalogProducts from './CatalogProducts/CatalogProducts.tsx';
+import { searchMovie } from './Api/Api.tsx';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children:[
+      {
+        path: "movie/:id",
+        element: <ProductDetailed  />
+    
+      },
+    ]
+  },
+  {
+    path: "*",
+    element: <NotFound />
+  },
+ 
+]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary fallback={<ErrorMessage />}>
-      <App />
-    </ErrorBoundary>
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
