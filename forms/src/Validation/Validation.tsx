ec233e2915b6aee6c605bbb6ee5a995af4fa198e
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-12-06 17:36:00 +0100

    refactor:  change password validation

--- forms/src/Validation/Validation.tsx
+++ forms/src/Validation/Validation.tsx
@@ -21,15 +21,30 @@ export const schema = yup.object().shape({
     .string()
     .required("Password is a required field")
     .min(8, "Password must be at least 8 characters")
-    .test("password-strength", "Password is not strong enough", (value) => {
-      if (!value) return true;
+    .test('password-complexity', function (value: string = ''):
+      | true
+      | yup.ValidationError {
+      const lowercaseRegex = /[a-z]/;
+      const uppercaseRegex = /[A-Z]/;
+      const numberRegex = /[0-9]/;
+      const symbolRegex = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/\-]/;
 
-      const hasUppercase = /[A-Z]/.test(value);
-      const hasLowercase = /[a-z]/.test(value);
-      const hasDigit = /\d/.test(value);
-      const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+      const errors = [];
 
-      return hasUppercase && hasLowercase && hasDigit && hasSpecialChar;
+      if (!lowercaseRegex.test(value))
+        errors.push('one lowercase letter in latin');
+      if (!uppercaseRegex.test(value))
+        errors.push('one capital letter in latin');
+      if (!numberRegex.test(value)) errors.push('one digit');
+      if (!symbolRegex.test(value)) errors.push('one special character');
+
+      if (errors.length > 0) {
+        return this.createError({
+          message: `password complexity - ${4 - errors.length
+            }/4: password must contain at least ${errors.join(', ')}`,
+        });
+      }
+      return true;
     }),
   confirmPassword: yup
     .string()
